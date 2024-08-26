2881baf87a34f89fc24668056ba18b8c6260592d
AhnEunJin <modic970515@gmail.com>
2024-08-26 23:47:31 +0900

    left pane UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Col, Container, Row } from 'react-bootstrap';
 import './App.css';
+import ContactForm from './components/ContactForm';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className='title'>연락처</h1>
+      <Container>
+        <Row>
+          <Col>
+            <ContactForm />
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
